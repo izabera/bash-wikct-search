04aa480da02bc8f29dbeec50f91b53f2bdba94c9
izabera <izaberina@gmail.com>
2014-08-13 11:32:19 +0200

    made sed faster and less stupid
    maybe removing ^ and $ makes it even faster? not sure...

--- wikt.sh
+++ wikt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-TEMPVAR=$(grep -Pe "^\d+ $1$" titles -m 1 -n | sed 's/^\([0-9]\+\):\([0-9]\+\) .*$/\1 \2/g')
-MATCHROW=$(echo $TEMPVAR | sed 's/^.* //')
-TEMPNUM=$(echo $TEMPVAR | sed 's/ .*$//')
+TEMPVAR=$(grep -Pe "^\d+ $1$" titles -m 1 -n | sed 's/ .*$//g')
+MATCHROW=$(echo $TEMPVAR | sed 's/^.*://')
+TEMPNUM=$(echo $TEMPVAR | sed 's/:.*$//')
 NEXTROW=$(head titles -n $((TEMPNUM+1)) | tail -n 1 | sed 's/ .*//')
 head enwiktionary-latest-pages-articles.xml -n "$NEXTROW" | tail -n $((NEXTROW - MATCHROW -1))>result
 grep '#' result -m 1 | sed -e 's/^..//' -e 's/\[//g' -e 's/\]//g' -e 's/ See also.*//'
